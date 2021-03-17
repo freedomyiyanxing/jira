3e7beb4c7e28581290771c1b85f7c6b0effc2cd1
freedom.yi <851989962freedom.yi@gmail.com>
2021-03-17 20:49:30 +0800

    ci: test commitlint.config

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,7 +1,12 @@
+/*
+ * commitlint 配置文件
+ * https://commitlint.js.org/#/reference-configuration
+ */
 const Configuration = {
+  extends: ['@commitlint/config-conventional'],
   rules: {
     'header-max-length': [2, 'always', 100],
-    'type-enum': [2, 'always', ['feat', 'fix', 'docs', 'style', 'refactor', 'test', 'revert']],
+    'type-enum': [2, 'always', ['ci', 'feat', 'fix', 'docs', 'style', 'refactor', 'test', 'revert']],
   },
 };
 
