1fb7227cdb06779bb1e600ea3dae77a089dc222c
freedom.yi <851989962freedom.yi@gmail.com>
2021-03-17 20:46:12 +0800

    fix

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,6 +1,7 @@
 const Configuration = {
   rules: {
     'header-max-length': [2, 'always', 100],
+    'type-enum': [2, 'always', ['feat', 'fix', 'docs', 'style', 'refactor', 'test', 'revert']],
   },
 };
 
